2c1e780a4fa9a9ff387f80c442d01bdcd20b1fdb
Simiola <70043967+Simiola@users.noreply.github.com>
2021-12-12 17:30:59 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import PickImage from './screens/camera';
+
+export default class App extends React.Component{
+  render(){
+    return <PickImage/>
+  }
+}
